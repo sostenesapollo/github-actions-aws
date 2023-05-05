531a396e5ba981aa69eeb76bd4c322388a88d143
Sostenes Apollo <ctr.sostenes.dasilva@prth.com>
2023-05-05 16:10:22 -0300

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('express')()
 const PORT = 3000
 
-app.get('/', (req, res)=> res.send('running v3'))
+app.get('/', (req, res)=> res.send('running v4))
 
 app.listen(PORT, ()=> console.log(`Running at port ${PORT}`))
